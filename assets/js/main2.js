8a9d6f5c77a7dde3e9c6cd9bd513f69b2a4a27d7
davidedallecarbonare94 <davide.dallecarbonare94@gmail.com>
2021-05-10 18:40:37 +0200

    start js snack 5

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -20,7 +20,7 @@ Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 */
 
-var numbers = [1, 2, 3];
+/* var numbers = [1, 2, 3];
 var letters = ["a", "b", "c"];
 
 function fuseMe(numbers, letters) {
@@ -40,4 +40,12 @@ function fuseMe(numbers, letters) {
 }
 
 var newarray = fuseMe(numbers, letters);
-console.log(newarray);
+console.log(newarray); */
+
+/* 
+Snack 5:
+Scrivi una funzione che accetti tre argomenti:
+un array e due numeri 
+//(“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
+*/
